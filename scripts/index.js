cb4c8196ba1b9b715ddfca14848f598033aea630
SAM BINGHAM <sambingham3143@gmail.com>
2024-10-09 17:42:27 -0600

    lazy map?

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,21 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('show'); // Toggle the menu visibility
     hamButton.classList.toggle('open'); // Toggle the open class
 });
+
+function loadMap() {
+    const mapIframe = document.getElementById('map');
+    const googleMapUrl = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10011.546582649106!2d-111.7852206!3d43.8260737!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8762af3f5a92e9ef%3A0x3d6e6a4d24cf9197!2sRexburg%2C%20ID%2098340%2C%20USA!5e0!3m2!1sen!2sus!4v1693439384854!5m2!1sen!2sus";
+    mapIframe.src = googleMapUrl;
+}
+
+const mapContainer = document.getElementById('map-container');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            loadMap();
+            observer.unobserve(mapContainer);
+        }
+    });
+});
+
+observer.observe(mapContainer);
