fd382f22378fa4bac182ebc09f464fce2d409dc3
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2024-11-08 11:08:57 -0700

    fixes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,8 +1,8 @@
 const tempID = document.getElementsByClassName('temperature')[0];
-const windspeedID = document.querySelector('.windspeed');
+const windspeedID = document.getElementsByClassName('windspeed')[0];
 
 const temp = parseFloat(tempID.innerText);
-const windspeed = parseFloat(windspeedID.getAttribute('data-value'));
+const windspeed = parseFloat(windspeedID.innerText);
 
 const windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
 const windchillInt = parseInt(windchill);
