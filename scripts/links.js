a4150bae24c55dd318b4e5ad82a4640bd3255654
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2024-11-14 14:02:36 -0700

    links

--- scripts/links.js
+++ scripts/links.js
@@ -5,19 +5,17 @@ const links = document.querySelector('#lesson-links');
 const displayLinks = (weeks) => {
     weeks.forEach((week) => {
         let lessonSection = document.createElement('section');
-        let lessonTitle = document.createElement('h4');
+        let lessonTitle = document.createElement('p');
         lessonTitle.textContent = `Lesson ${week.lesson}`;
         lessonSection.appendChild(lessonTitle);
 
         week.links.forEach((link) => {
-            let title = document.createElement('p');
             let url = document.createElement('a');
   
-            title.textContent = link.title;
+            // .textContent = link.title;
             url.href = baseURL + link.url;
-            url.textContent = link.url;
+            url.textContent = `| ${link.title} |`;
             
-            lessonSection.appendChild(title);
             lessonSection.appendChild(url);
         });
 
