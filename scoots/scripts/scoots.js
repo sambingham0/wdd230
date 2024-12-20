697cf140f025341c8c1680686b33d48a87fc7ea4
Sam Bingham <131708684+sambingham0@users.noreply.github.com>
2024-12-19 19:35:16 -0700

    fix

--- scoots/scripts/scoots.js
+++ scoots/scripts/scoots.js
@@ -55,21 +55,21 @@ async function forecastAPIFetch() {
 
 function displayResults(data) {
   const temp = Math.round(data.main.temp);
-  currentTemp.innerHTML = `${temp}&deg;F`;
+  currentTemp.textContent = `${temp}&deg;F`;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   let title = data.weather[0].main;
   let desc = data.weather[0].description;
-  weatherTitle.innerHTML = title;
+  weatherTitle.textContent = title;
 
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = `${desc}`;
 
   const humid = data.main.humidity;
-  currentHumid.innerHTML = `${humid}%`;
+  currentHumid.textContent = `${humid}%`;
 
   const high = data.main.temp_max;
-  tempHigh.innerHTML = `Today's high temperature: ${high}&deg;F`;
+  tempHigh.textContent = `Today's high temperature: ${high}&deg;F`;
 }
 
 function displayForecast(forecastList) {
@@ -86,7 +86,7 @@ function displayForecast(forecastList) {
   );
 
   const temp = Math.round(tomorrowForecast.main.temp);
-  forecast.innerHTML = `${temp}&deg;F`;
+  forecast.textContent = `${temp}&deg;F`;
 }
 
 // rentals scripts
